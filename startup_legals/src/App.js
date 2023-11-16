905b20e6d8278ed71d8cef495f145b19267cf9e2
keabt <keabhello@gmail.com>
2023-11-16 10:32:19 +0000

    Project files
    Empty project files

---
+++ startup_legals/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <h1>
+          Startup Legals
+        </h1>
+        <a
+          className="App-link"
+          href="https://www.rocketlawyer.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Create contract 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
